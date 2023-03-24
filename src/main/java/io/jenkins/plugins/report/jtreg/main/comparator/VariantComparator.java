a7486ee90cac23a89ce58787f4547ade64967af3
Patrik Cerbak <patrik.cerbak@seznam.cz>
2023-03-24 10:56:38 +0100

    refactored and added the option for printing the query from failed builds hashmap(--slim)

--- src/main/java/io/jenkins/plugins/report/jtreg/main/comparator/VariantComparator.java
+++ src/main/java/io/jenkins/plugins/report/jtreg/main/comparator/VariantComparator.java
@@ -10,39 +10,19 @@ public class VariantComparator {
         Options options = Arguments.parse(args);
         Jobs jobs = new Jobs(options.getJobsPath());
 
-        if (options.getOperation() == Options.Operations.List) {
-            ArrayList<File> jobsToCompare = jobs.getJobsByQuery(options.getQueryString());
-            ArrayList<File> buildsToCompare = new ArrayList<>();
-            for (File job : jobsToCompare) {
-                ArrayList<File> builds = Builds.getBuilds(job, options.isSkipFailed(), options.getNvrQuery());
-                buildsToCompare.addAll(builds);
-            }
+        ArrayList<File> jobsToCompare = jobs.getJobsByQuery(options.getQueryString());
+        ArrayList<File> buildsToCompare = new ArrayList<>();
+        for (File job : jobsToCompare) {
+            ArrayList<File> builds = Builds.getBuilds(job, options.isSkipFailed(), options.getNvrQuery());
+            buildsToCompare.addAll(builds);
+        }
 
-            Tests.printFailedTable(Tests.createFailedMap(buildsToCompare));
+        if (options.getOperation() == Options.Operations.List || options.getOperation() == Options.Operations.Compare) {
+            Tests.printFailedTable(Tests.createFailedMap(buildsToCompare), options.getOperation());
         } else if (options.getOperation() == Options.Operations.Enumerate) {
             jobs.printVariants(options.getQueryString());
-        } else if (options.getOperation() == Options.Operations.Compare) {
-            ArrayList<String> commandArguments = new ArrayList<>();
-
-            ArrayList<File> jobsToCompare = jobs.getJobsByQuery(options.getQueryString());
-            for (File job : jobsToCompare) {
-                ArrayList<File> builds = Builds.getBuilds(job, options.isSkipFailed(), options.getNvrQuery());
-                for (File build : builds) {
-                    commandArguments.add(build.getAbsolutePath());
-                }
-            }
-
-            // add more arguments
-            commandArguments.add("--view=diff-list");
-            commandArguments.add("--view=info");
-            commandArguments.add("--view=info-hide-details");
-
-            // call the main method from jenkins-report-jck
-            try {
-                CompareBuilds.main(commandArguments.toArray(new String[0]));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        } else if (options.getOperation() == Options.Operations.Slim) {
+            Tests.printQueryForTest(options.getTest(), Tests.createFailedMap(buildsToCompare));
         }
     }
 }
