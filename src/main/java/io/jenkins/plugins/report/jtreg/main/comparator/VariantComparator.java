4171b8b214573568b921e76e906d8e099a531581
Patrik Cerbak <patrik.cerbak@seznam.cz>
2023-03-29 16:17:22 +0200

    updated the arguments of getBuilds method and deleted the slim option for now (not working properly)

--- src/main/java/io/jenkins/plugins/report/jtreg/main/comparator/VariantComparator.java
+++ src/main/java/io/jenkins/plugins/report/jtreg/main/comparator/VariantComparator.java
@@ -1,19 +1,17 @@
 package io.jenkins.plugins.report.jtreg.main.comparator;
 
-import io.jenkins.plugins.report.jtreg.main.diff.CompareBuilds;
-
 import java.io.File;
 import java.util.ArrayList;
 
 public class VariantComparator {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Options options = Arguments.parse(args);
         Jobs jobs = new Jobs(options.getJobsPath());
 
         ArrayList<File> jobsToCompare = jobs.getJobsByQuery(options.getQueryString());
         ArrayList<File> buildsToCompare = new ArrayList<>();
         for (File job : jobsToCompare) {
-            ArrayList<File> builds = Builds.getBuilds(job, options.isSkipFailed(), options.getNvrQuery());
+            ArrayList<File> builds = Builds.getBuilds(job, options.isSkipFailed(), options.getNvrQuery(), options.getNumberOfBuilds());
             buildsToCompare.addAll(builds);
         }
 
@@ -21,8 +19,6 @@ public class VariantComparator {
             Tests.printFailedTable(Tests.createFailedMap(buildsToCompare), options.getOperation());
         } else if (options.getOperation() == Options.Operations.Enumerate) {
             jobs.printVariants(options.getQueryString());
-        } else if (options.getOperation() == Options.Operations.Slim) {
-            Tests.printQueryForTest(options.getTest(), Tests.createFailedMap(buildsToCompare));
         }
     }
 }
