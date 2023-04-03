a63ff0a2bb2a7663011c1dec70619bf0a19bc503
Patrik Cerbak <patrik.cerbak@seznam.cz>
2023-04-03 16:57:51 +0200

    added the "!" (all except this) operand to query string and caching of the queryList

--- src/main/java/io/jenkins/plugins/report/jtreg/main/comparator/QueryString.java
+++ src/main/java/io/jenkins/plugins/report/jtreg/main/comparator/QueryString.java
@@ -2,24 +2,43 @@ package io.jenkins.plugins.report.jtreg.main.comparator;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class QueryString {
+    private static ArrayList<String[]> cachedQueryList = null;
+
     // parses the given query string into a list of string arrays
     private static ArrayList<String[]> parseToList(String queryString) {
         String[] queryArray = queryString.split("\\s+");
         ArrayList<String[]> queryList = new ArrayList<>();
 
         for (String s : queryArray) {
+            boolean reversed = false;
+            if (s.charAt(0) == '!') {
+                reversed = true;
+                s = s.substring(1);
+            }
             // checks for array in the query
             if (s.charAt(0) == '{') {
                 if (s.charAt(s.length() - 1) != '}') {
                     throw new RuntimeException("Expected closing }.");
                 }
                 String[] array = s.substring(1, s.length() - 1).split(",");
-                queryList.add(array);
+                if (reversed) {
+                    for (int i = 0; i < array.length; i++) {
+                        array[i] = "!" + array[i];
+                    }
+                    queryList.add(array);
+                } else {
+                    queryList.add(array);
+                }
             } else {
                 String[] arrayOfOne = new String[1];
-                arrayOfOne[0] = s;
+                if (reversed) {
+                    arrayOfOne[0] = "!" + s;
+                } else {
+                    arrayOfOne[0] = s;
+                }
                 queryList.add(arrayOfOne);
             }
         }
@@ -30,23 +49,24 @@ public class QueryString {
     static boolean checkJobWithQuery(File job, String queryString) {
         if (!queryString.equals("")) {
             String[] jobArray = job.getName().split("[.-]");
-            ArrayList<String[]> queryList = parseToList(queryString);
+            // caching the queryList to not call parseToList() every time
+            if (cachedQueryList == null) {
+                cachedQueryList = parseToList(queryString);
+            }
 
-            for (int i = 0; i < queryList.size(); i++) {
-                String[] qA = queryList.get(i); // get a single array from the queryList
+            for (int i = 0; i < cachedQueryList.size(); i++) {
+                String[] qA = cachedQueryList.get(i); // get a single array from the queryList
+
+                boolean reversed = qA[0].charAt(0) == '!'; // check for reversed query
+
                 if (qA.length == 1) {
-                    if (!qA[0].equals("*") && !qA[0].equals(jobArray[i])) {
+                    if ( (!reversed && !qA[0].equals("*") && !qA[0].equals(jobArray[i])) ||
+                            (reversed && !qA[0].equals("*") && qA[0].equals("!" + jobArray[i])) ) {
                         return false;
                     }
                 } else {
-                    boolean doesContain = false;
-                    for (String s : qA) {
-                        if (s.equals(jobArray[i])) {
-                            doesContain = true;
-                            break;
-                        }
-                    }
-                    if (!doesContain) {
+                    if ( (!reversed && !Arrays.asList(qA).contains(jobArray[i])) ||
+                            (reversed && Arrays.asList(qA).contains("!" + jobArray[i])) ) {
                         return false;
                     }
                 }
