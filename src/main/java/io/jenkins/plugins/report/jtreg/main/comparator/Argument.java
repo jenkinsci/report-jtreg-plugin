5c1e869082ab08b465984d068fdb7b051829fb0b
Patrik Cerbak <patrik.cerbak@seznam.cz>
2023-05-05 11:24:06 +0200

    remade the Argument class to not use Optional, it is simpler and should make more sense now

--- src/main/java/io/jenkins/plugins/report/jtreg/main/comparator/Argument.java
+++ src/main/java/io/jenkins/plugins/report/jtreg/main/comparator/Argument.java
@@ -1,17 +1,14 @@
 package io.jenkins.plugins.report.jtreg.main.comparator;
 
-import java.util.Optional;
-
 public class Argument {
-    private final String argumentName;
-    private final String helpMessage;
+    private final String argumentName; // the name of the argument (example: --path)
+    private final String helpMessage; // the help for the argument that will be showed in help message
+    private final String argumentFormat; // the "format/usage" of the argument that will be showed in the help message (example: <path/to/jobs> <number> etc.)
 
-    private final String argumentFormat;
-
-    public Argument(String argumentName, String helpMessage, Optional<String> argumentFormat) {
+    public Argument(String argumentName, String helpMessage, String argumentFormat) {
         this.argumentName = argumentName;
         this.helpMessage = helpMessage;
-        this.argumentFormat = argumentFormat.orElse("");
+        this.argumentFormat = argumentFormat;
     }
 
     public String getName() {
--- src/main/java/io/jenkins/plugins/report/jtreg/main/comparator/ArgumentsDeclaration.java
+++ src/main/java/io/jenkins/plugins/report/jtreg/main/comparator/ArgumentsDeclaration.java
@@ -1,19 +1,18 @@
 package io.jenkins.plugins.report.jtreg.main.comparator;
 
-import java.util.Optional;
-
 public final class ArgumentsDeclaration {
-    public static final Argument helpArg = new Argument("--help", "Print this help message.", Optional.empty());
-    public static final Argument listArg = new Argument("--list", "Print a table of matched jobs with their builds and the tests that failed in the build.", Optional.empty());
-    public static final Argument enumerateArg = new Argument("--enumerate", "Print lists of all variants of jobs (that match the rest of arguments).", Optional.empty());
-    public static final Argument compareArg = new Argument("--compare", "Print a table of all failed tests (of matched job builds) and the builds where they failed.", Optional.empty());
-    public static final Argument printArg = new Argument("--print", "Print all jobs and their builds that match the rest of arguments, without actually doing any operation on the builds or tests.", Optional.empty());
-    public static final Argument pathArg = new Argument("--path", "A system path to a directory with your jenkins jobs.", Optional.of(" <path/to/jenkins/jobs>"));
-    public static final Argument queryArg = new Argument("--query", "A query string to filter the jobs (the syntax is described below).", Optional.of(" <querystring>"));
-    public static final Argument nvrArg = new Argument("--nvr", "To specify what builds to take (only builds with specified NVRs). The syntax is described below.", Optional.of(" <nvrquery>"));
-    public static final Argument historyArg = new Argument("--history", "To specify the maximum number of builds to look in.", Optional.of(" <number>"));
-    public static final Argument skipFailedArg = new Argument("--skip-failed", "Specify whether the comparator should skip failed tests (only take successful and unstable) or take all. The default value is true.", Optional.of("=<true/false>"));
-    public static final Argument forceArg = new Argument("--force", "Used for forcing vague requests, that could potentially take a long time.", Optional.empty());
-    public static final Argument exactLengthArg = new Argument("--exact-length", "Since job names can have different lengths and sometimes we only need to match jobs with exact name length (number of element in the job name), this specifies the exact length.", Optional.of(" <number>"));
-    public static final Argument onlyVolatileArg = new Argument("--only-volatile", "Specify true to show only non stable tests with the arguments list and compare (shows only tests, that are NOT failed everywhere). The default value is false.", Optional.of("=<true/false>"));
+  //public static final Argument exampleArg = new Argument("--argument-name", "The help message of this argument that will be shown with --help.", "<format/usage of argument showed with --help>")
+    public static final Argument helpArg = new Argument("--help", "Print this help message.", "");
+    public static final Argument listArg = new Argument("--list", "Print a table of matched jobs with their builds and the tests that failed in the build.", "");
+    public static final Argument enumerateArg = new Argument("--enumerate", "Print lists of all variants of jobs (that match the rest of arguments).", "");
+    public static final Argument compareArg = new Argument("--compare", "Print a table of all failed tests (of matched job builds) and the builds where they failed.", "");
+    public static final Argument printArg = new Argument("--print", "Print all jobs and their builds that match the rest of arguments, without actually doing any operation on the builds or tests.", "");
+    public static final Argument pathArg = new Argument("--path", "A system path to a directory with your jenkins jobs.", " <path/to/jenkins/jobs>");
+    public static final Argument queryArg = new Argument("--query", "A query string to filter the jobs (the syntax is described below).", " <querystring>");
+    public static final Argument nvrArg = new Argument("--nvr", "To specify what builds to take (only builds with specified NVRs). The syntax is described below.", " <nvrquery>");
+    public static final Argument historyArg = new Argument("--history", "To specify the maximum number of builds to look in.", " <number>");
+    public static final Argument skipFailedArg = new Argument("--skip-failed", "Specify whether the comparator should skip failed tests (only take successful and unstable) or take all. The default value is true.", "=<true/false>");
+    public static final Argument forceArg = new Argument("--force", "Used for forcing vague requests, that could potentially take a long time.", "");
+    public static final Argument exactLengthArg = new Argument("--exact-length", "Since job names can have different lengths and sometimes we only need to match jobs with exact name length (number of element in the job name), this specifies the exact length.", " <number>");
+    public static final Argument onlyVolatileArg = new Argument("--only-volatile", "Specify true to show only non stable tests with the arguments list and compare (shows only tests, that are NOT failed everywhere). The default value is false.", "=<true/false>");
 }
