10b6b841969e8a42ab113db3e9cf52e86f895582
Jiri <jvanek@redhat.com>
2023-06-10 19:39:35 +0200

    Downgraded to chartjs-1
    project page
    its events
    
    view
    its events
    
    pom

--- src/main/resources/io/jenkins/plugins/report/jtreg/ReportChartColumn/chartLogicColumn.js
+++ src/main/resources/io/jenkins/plugins/report/jtreg/ReportChartColumn/chartLogicColumn.js
@@ -20,59 +20,49 @@ function readJcks() {
                   var jckChartNameVar = {};
                 }
                 var dataJckView = {
-                  type: 'line',
-                  url_from_job: data_url,
-                  data:  {
                     labels: data_builds,
                             datasets: [
                             {
                             label: "Error",
-                            fill: true,
-                            backgroundColor: "rgba(255,0,255,0.2)",
-                            borderColor: "rgba(255,0,255,1)",
-                            pointBackgroundColor: "rgba(255,0,255,1)",
-                            pointBorderColor: "#fff",
-                            pointHoverBackgroundColor: "#fff",
-                            pointHoverBorderColor: "rgba(255,0,255,1)",
-                            pointRadius: 4,
+                            fillColor: "rgba(255,0,255,0.2)",
+                            strokeColor: "rgba(255,0,255,1)",
+                            pointColor: "rgba(255,0,255,1)",
+                            pointStrokeColor: "#fff",
+                            pointHighlightFill: "#fff",
+                            pointHighlightStroke: "rgba(255,0,255,1)",
                                     data: data_error
                             },
                             {
                             label: "Failed",
-                            fill: true,
-                            backgroundColor: "rgba(255,0,0,0.2)",
-                            borderColor: "rgba(255,0,0,1)",
-                            pointBackgroundColor: "rgba(255,0,0,1)",
-                            pointBorderColor: "#fff",
-                            pointHoverBackgroundColor: "#fff",
-                            pointHoverBorderColor: "rgba(255,0,0,1)",
-                            pointRadius: 4,
+                            fillColor: "rgba(255,0,0,0.2)",
+                            strokeColor: "rgba(255,0,0,1)",
+                            pointColor: "rgba(255,0,0,1)",
+                            pointStrokeColor: "#fff",
+                            pointHighlightFill: "#fff",
+                            pointHighlightStroke: "rgba(255,0,0,1)",
                                     data: data_failed
                             }
                             ]
-                    },
-                    options: {
-                        plugins: {
-                            legend: { display: false }
-                        },
-                        interaction: {
-                            mode: 'index',
-                            intersect: false
-                        },
-                        onClick: (e) => {
-                            var chart = e.chart;
-                            var activePoints = chart.getElementsAtEventForMode(e, 'index', { intersect: false }, true);
+                    };
+                    var options = {
+                        url_from_job: data_url,
+                        bezierCurve: false,
+                        multiTooltipTemplate: "<%= datasetLabel + \": \" + value %>"
+                    }
+                    var ctx = document.getElementById("jckChart-"+id).getContext("2d");
+                    jckChartNameVar[id]  = new Chart(ctx).Line(dataJckView, options);
+                    document.getElementById("jckChartContainer-"+id).onclick = function (evt) {
+                            var lid = event.target.id;
+                            var jid = lid.replace("jckChart-", "")
+                            var chart = jckChartNameVar[jid]
+                            var activePoints = chart.getPointsAtEvent(evt);
                             var point = activePoints[0]
                             var datasetIndex = point.datasetIndex //labels are for all data together,  no need to look into exact dataset
                             var index = point.index
-                            var result = chart.config.data.labels[index]
+                            var result = point.label
                             var buildId = result.substring(result.lastIndexOf(":") + 1) //thsi works even without :; fixme, add NVR?
-                            window.open("/"+chart.config._config.url_from_job+buildId+"/java-reports", "_blank");
-                    }
-                    }
-                };
-                    var ctx = document.getElementById("jckChart-"+id).getContext("2d");
-                    jckChartNameVar[id]  = new Chart(ctx, dataJckView);
+                            window.open("/"+chart.options.url_from_job+buildId+"/java-reports", "_blank");
+                        };
 
     }
 }
