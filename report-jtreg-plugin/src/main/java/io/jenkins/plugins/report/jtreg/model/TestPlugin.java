5238b95a4f986c8354a918bdcc32766c7a76177d
Patrik Cerbak <patrik.cerbak@seznam.cz>
2023-09-27 16:59:45 +0200

    made the Test class in plugin extend the Test class in lib and renamed it (also changed Report to use generics)

--- report-jtreg-plugin/src/main/java/io/jenkins/plugins/report/jtreg/model/TestPlugin.java
+++ report-jtreg-plugin/src/main/java/io/jenkins/plugins/report/jtreg/model/TestPlugin.java
@@ -3,42 +3,21 @@ package io.jenkins.plugins.report.jtreg.model;
 import java.util.List;
 import java.util.Objects;
 
-import io.jenkins.plugins.report.jtreg.utils.MoreStrings;
-
-public class Test implements Comparable<Test>, java.io.Serializable {
+public class TestPlugin extends Test {
 
     private final String name;
     private final TestStatus status;
     private final String statusLine;
     private final List<TestOutput> outputs;
 
-    public Test(String name, TestStatus status, String statusLine, List<TestOutput> outputs) {
+    public TestPlugin(String name, TestStatus status, String statusLine, List<TestOutput> outputs) {
+        super(name, status, statusLine, outputs);
         this.name = name;
         this.status = status;
         this.statusLine = statusLine;
         this.outputs = outputs;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public TestStatus getStatus() {
-        return status;
-    }
-
-    public String getStatusLine() {
-        return statusLine;
-    }
-
-    public List<TestOutput> getOutputs() {
-        return outputs;
-    }
-
-    @Override
-    public int compareTo(Test o) {
-        return MoreStrings.compareStrings(name, o.getName());
-    }
 
     @Override
     public boolean equals(Object o) {
@@ -48,7 +27,7 @@ public class Test implements Comparable<Test>, java.io.Serializable {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        Test test = (Test) o;
+        TestPlugin test = (TestPlugin) o;
         return Objects.equals(name, test.name) && status == test.status && Objects.equals(statusLine, test.statusLine) && Objects.equals(outputs, test.outputs);
     }
 
@@ -58,16 +37,9 @@ public class Test implements Comparable<Test>, java.io.Serializable {
         return Objects.hash(name, status, statusLine, outputs);
     }
 
-    @Override
-    public String toString() {
-        return "Test{" + "name='" + name + '\'' + ", status=" + status + ", statusLine='" + statusLine + '\'' + ", " +
-                "outputs=" + outputs + '}';
-    }
-
     private static String createDiffUrl() {
         return SuiteTestsWithResultsPlugin.getDiffServer() + "?generated-part=+-view%3Dall-tests+++-output%3Dhtml++-fill++";
     }
-
 
     public String getTrackingUrl(String job) {
         return (createDiffUrl()+"&custom-part=-track%3D"+name+"++"+job+"++0+-365").replaceAll("#", "%23");
