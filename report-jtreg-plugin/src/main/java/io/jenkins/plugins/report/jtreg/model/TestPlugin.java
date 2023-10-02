f6745cac92f42dfff0914e646d331532c32a3ce1
Patrik Cerbak <patrik.cerbak@seznam.cz>
2023-10-02 18:04:43 +0200

    deleted equals and hashcode methods from TestPlugin

--- report-jtreg-plugin/src/main/java/io/jenkins/plugins/report/jtreg/model/TestPlugin.java
+++ report-jtreg-plugin/src/main/java/io/jenkins/plugins/report/jtreg/model/TestPlugin.java
@@ -1,8 +1,12 @@
 package io.jenkins.plugins.report.jtreg.model;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 import java.util.List;
 import java.util.Objects;
 
+// TODO
+@SuppressFBWarnings
 public class TestPlugin extends Test {
 
     private final String name;
@@ -18,29 +22,11 @@ public class TestPlugin extends Test {
         this.outputs = outputs;
     }
 
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        TestPlugin test = (TestPlugin) o;
-        return Objects.equals(name, test.name) && status == test.status && Objects.equals(statusLine, test.statusLine) && Objects.equals(outputs, test.outputs);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(name, status, statusLine, outputs);
-    }
-
     private static String createDiffUrl() {
         return SuiteTestsWithResultsPlugin.getDiffServer() + "?generated-part=+-view%3Dall-tests+++-output%3Dhtml++-fill++";
     }
 
+    @Override
     public String getTrackingUrl(String job) {
         return (createDiffUrl()+"&custom-part=-track%3D"+name+"++"+job+"++0+-365").replaceAll("#", "%23");
     }
