aadcc472831f51d39bac9eb2ced9111fed6fd04b
Patrik Cerbak <patrik.cerbak@seznam.cz>
2024-03-18 16:33:13 +0100

    refactored the printing of the compare table - now showing also passed builds

--- report-jtreg-comparator/src/main/java/io/jenkins/plugins/report/jtreg/main/comparator/FailedTests.java
+++ report-jtreg-comparator/src/main/java/io/jenkins/plugins/report/jtreg/main/comparator/FailedTests.java
@@ -86,68 +86,66 @@ public class FailedTests {
         return failedMap;
     }
 
-    // function for reversing the HashMap from reverseFailedMap() method
-    // Keys: Failed test names, Values: list of builds where the test failed
-    private static HashMap<String, ArrayList<String>> reverseFailedMap(HashMap<String, ArrayList<String>> failedMap) {
-        HashMap<String, ArrayList<String>> reversedMap = new HashMap<>();
-
-        ArrayList<String> allFailed = new ArrayList<>();
-        for (ArrayList<String> tests : failedMap.values()) {
-            for (String test : tests) {
-                if(!allFailed.contains(test)) {
-                    allFailed.add(test);
-                }
-            }
-        }
-
-        for (String test : allFailed) {
-            ArrayList<String> testBuilds = new ArrayList<>();
-            for (Map.Entry<String, ArrayList<String>> entry : failedMap.entrySet()) {
-                if (entry.getValue().contains(test)) {
-                    testBuilds.add(entry.getKey());
-                }
-            }
-            reversedMap.put(test, testBuilds);
-        }
-
-        return reversedMap;
-    }
-
     // function for getting the HashMap of failed tests ready for printing to console
     public static void printFailedTable(HashMap<String, ArrayList<String>> failedMap, Options.Operations operation, Formatter formatter) {
-        if (operation == Options.Operations.Compare) {
-            failedMap = reverseFailedMap(failedMap);
+        // get all non-duplicate failed tests from the map
+        Set<String> nonRepeatingValues = new HashSet<>();
+        for (ArrayList<String> values : failedMap.values()) {
+            nonRepeatingValues.addAll(values);
         }
 
-        ArrayList<String> allFailedTests = new ArrayList<>();
-        for (ArrayList<String> tests : failedMap.values()) {
-            for (String test : tests) {
-                if(!allFailedTests.contains(test)) {
-                    allFailedTests.add(test);
-                }
-            }
-        }
-        Collections.sort(allFailedTests);
+        // sort the failed tests set
+        List<String> sortedValues = new ArrayList<>(nonRepeatingValues);
+        Collections.sort(sortedValues);
 
-        String[][] table = new String[failedMap.size() + 1][allFailedTests.size() + 1];
-        // add first line (header)
-        for (int i = 1; i < allFailedTests.size() + 1; i++) {
-            table[0][i] = allFailedTests.get(i - 1);
-        }
-        // add the builds and tests
-        ArrayList<String> keys = new ArrayList<>(failedMap.keySet());
+        // get the key from the map (builds) and sort them
+        List<String> keys = new ArrayList<>(failedMap.keySet());
         Collections.sort(keys);
 
-        // add the "X"s to the table
+        // if the operation is "compare" (rows are failed tests and columns builds), switch the values and keys
+        if (operation == Options.Operations.Compare) {
+            List<String> temp = keys;
+            keys = sortedValues;
+            sortedValues = temp;
+        }
+
+        // create the table itself (2D array), where [rows][columns]
+        String[][] table = new String[keys.size() + 1][sortedValues.size() + 1];
+
+        // first, put values into the table header (first row)
+        for (int i = 1; i < sortedValues.size() + 1; i++) {
+            table[0][i] = sortedValues.get(i - 1);
+        }
+
+        // add the "X"s to the table where the tests fail
         int i = 1;
         for (String key : keys) {
-            table[i][0] = key;
-            for (String test : failedMap.get(key)) {
-                table[i][allFailedTests.indexOf(test) + 1] = "X";
+            table[i][0] = key; // put the key into first column
+
+            // now, create a list of values where to put the X for each column
+            List<String> putXList = new ArrayList<>();
+            if (operation == Options.Operations.Compare) {
+                // if the operation is compare, the values to put X are the builds (or the keys in the map),
+                // so it has to go through the map and find them
+                for (Map.Entry<String, ArrayList<String>> entry : failedMap.entrySet()) {
+                    if (entry.getValue().contains(key)) {
+                        putXList.add(entry.getKey());
+                    }
+                }
+            } else {
+                // otherwise, just get the values from the map
+                putXList = failedMap.get(key);
             }
+
+            // put the Xs itself
+            for (String value : putXList) {
+                table[i][sortedValues.indexOf(value) + 1] = "X";
+            }
+
             i++;
         }
 
-        formatter.printTable(table, failedMap.size() + 1, allFailedTests.size() + 1);
+        // print the table into stdout
+        formatter.printTable(table, keys.size() + 1, sortedValues.size() + 1);
     }
 }
