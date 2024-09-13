ed354249fd8fd352ad52f5c8051430845e867f3b
Jiri Novak <jinovak@jinovak-thinkpadt14sgen2i.tpb.csb>
2024-09-13 14:27:09 +0200

    WIP add link tests

---
+++ report-jtreg-comparator/src/test/java/io/jenkins/plugins/report/jtreg/main/comparator/StackTraceCompareTest.java
@@ -0,0 +1,13 @@
+package io.jenkins.plugins.report.jtreg.main.comparator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StackTraceCompareTest {
+
+    @Test
+    void removeSetReferential() {
+        
+    }
+}
